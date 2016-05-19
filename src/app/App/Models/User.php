89166d2404fd2669537c23be58fc4d8c8bd6cee3
D.Lo <WebDevDLo@gmail.com>
2016-05-18 22:35:55 -0400

    Add prepared statement for ::getByLogin()

--- src/app/App/Models/User.php
+++ src/app/App/Models/User.php
@@ -9,8 +9,11 @@ abstract class User
     public static function getByLogin(Container $c, array $data)
     {
         $db = $c->get('db');
-        var_dump($data); exit;
-        return true;
+        $statement = $db->prepare('SELECT * FROM users WHERE username = :username AND password = :password');
+        $statement->bindValue('username', $data['username'], \PDO::PARAM_STR);
+        $statement->bindValue('password', $data['password'], \PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetch();
     }
     
 }
