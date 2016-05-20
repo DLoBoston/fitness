84769ad3b66c4a0eed10ff89df11ae3fd21a5647
D.Lo <WebDevDLo@gmail.com>
2016-05-20 09:14:26 -0400

    Improve header markup with HTML5 organization. Make display of navigation dynamic for logged in users only.

--- src/templates/partials/header.php
+++ src/templates/partials/header.php
@@ -6,11 +6,14 @@
 <body>
     
 <header>
-    <nav>
-        <ul>
-            <li><a href="/my/logout">Logout</a></li>
-        </ul>
-    </nav>
+    <h1><a href="/">My Fitness App</a></h1>
+    <?php if (isset($_SESSION['userId'])): ?>
+        <!--  Only display navigation to logged in users  -->
+        <nav>
+            <ul>
+                <li><a href="/my/logout">Logout</a></li>
+            </ul>
+        </nav>
+    <?php endif; ?>
 </header>
     
-<h1><a href="/">My Fitness App</a></h1>
