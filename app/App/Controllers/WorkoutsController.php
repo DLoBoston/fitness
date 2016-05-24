df2c67695ebcd3be1acae70226a43a76b208247a
D.Lo <WebDevDLo@gmail.com>
2016-05-23 20:21:20 -0400

    Write WorkoutsController::showWorkouts logic to display workouts via template.

--- app/App/Controllers/WorkoutsController.php
+++ app/App/Controllers/WorkoutsController.php
@@ -6,6 +6,7 @@
 namespace App\Controllers;
 
 use \Slim\Container;
+use \App\Models\User;
 
 /**
  * Controller to manage interacting with workouts on the site via route requests.
@@ -30,14 +31,23 @@ class WorkoutsController
     }
     
     /**
-     * Display list of existing workouts. Provide links to CRUD actions.
+     * Display list of existing workouts for the logged in user via the user dashboard.
+     * User is found with SESSION['userId']. Provide links to CRUD actions.
      * 
      * @param \Slim\Http\Request $request PSR-7 Request
      * @param \Slim\Http\Response $response PSR-7 Response
      * @return \Slim\Http\Response PSR-7 Response
      */
     public function showWorkouts($request, $response) {
-        $response->getBody()->write("showWorkouts");
+        
+        // Connect to ORM
+        $this->container->get('orm');
+        
+        // Get the logged in user's workouts
+        $workouts = User::find($_SESSION['userId'])->workouts;
+        
+        // Display
+        $response = $this->container->get('view')->render($response, "dashboard.php", ["workouts" => $workouts]);
         return $response;
     }
     
--- templates/partials/workouts.php
+++ templates/partials/workouts.php
@@ -1,5 +1,9 @@
 <h3>Workouts</h3>
 
 <ul>
-    <li>Workout #1</li>
+    <?php
+        foreach ($workouts as $workout) :
+            echo '<li>' . date('l, F j, Y', strtotime($workout->workout_date)) . '</li>';
+        endforeach;
+    ?>
 </ul>
