daca17eaa6e40870b4009ec491282aebf9f0b2e3
D.Lo <WebDevDLo@gmail.com>
2016-05-24 07:40:53 -0400

    Update workouts partial template to display even if there are no workouts

--- templates/partials/workouts.php
+++ templates/partials/workouts.php
@@ -2,8 +2,12 @@
 
 <ul>
     <?php
-        foreach ($workouts as $workout) :
-            echo '<li>' . date('l, F j, Y', strtotime($workout->workout_date)) . '</li>';
-        endforeach;
+        if (isset($workouts)) :
+            foreach ($workouts as $workout) :
+                echo '<li>' . date('l, F j, Y', strtotime($workout->workout_date)) . '</li>';
+            endforeach;
+        else :
+            echo '<li>no workouts</li>';
+        endif;
     ?>
 </ul>
